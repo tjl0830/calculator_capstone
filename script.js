043e6c99aa971d0b83d96afe1de6d862f02aec58
tjl0830 <tjlbautista30@gmail.com>
2024-04-17 23:06:28 +0800

    Change some lines since may topak

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const display = document.getElementById("display");
 
+// Adds the value of each button press
 function appendToDisplay(input) {
     display.value+= input;
 }
 
+// Delete a character
 function deleteCharacter() {
     display.value = display.value.slice(0,-1)
 }
 
+// Clears the display
 function clearDisplay() {
     display.value = "";
 }
 
+// Calculate the display value
 function calculate() {
     try {
         display.value = eval(display.value);
